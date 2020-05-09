ba649cd9c69347464bb2be2583aa31aa1f44297b
Your Name <welwood32@gmail.com>
2020-05-09 12:07:58 -0400

    initial

---
+++ script.js
@@ -0,0 +1,2 @@
+var currentDateAndTime = Date(Date.now());// exp: Fri Oct 25 2019 17:40:56 GMT-0400 (Eastern Daylight Time); 
+var currentHour = new Date().getHours();//Current hour in military time
